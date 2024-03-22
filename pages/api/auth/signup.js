9ea2096b2e1df73547fae92ea7b515dc38a1dc8f
Назар Остафійчук <ostafii4uk@gmail.com>
2024-03-22 11:31:42 +0200

    Added submit handler for sign up form

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,22 +2,36 @@ import { hashPassword } from '../../../lib/auth'
 import { connectToDatabase } from '../../../lib/db'
 
 async function handler(req, res) {
-  const data = req.body
-  const { email, password } = data
+  if (req.method === 'POST') {
+    const data = req.body
+    const { email, password } = data
 
-  if (!email || !email.includes('@') || !password || !password.trim().length < 7) {
-    res.status(422).json({ message: 'Invalid input - password should also be at least 7 characters long.' })
-    return
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res
+        .status(422)
+        .json({
+          message:
+            'Invalid input - password should also be at least 7 characters long.'
+        })
+      return
+    }
+
+    const client = await connectToDatabase()
+    const db = client.db()
+
+    const hashedPassword = await hashPassword(password)
+
+    const result = await db
+      .collection('users')
+      .insertOne({ email, password: hashedPassword })
+
+    res.status(201).json({ message: 'Success created user!' })
   }
-
-  const client = await connectToDatabase()
-  const db = client.db()
-
-  const hashedPassword = hashPassword(password)
-
-  const result = await db.collection('users').insertOne({ email, password: hashedPassword })
-
-  res.status(201).json({ message: 'Success created user!' })
 }
 
 export default handler
