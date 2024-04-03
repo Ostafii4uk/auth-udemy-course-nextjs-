b570cc7c92441b2ffc012eadabd87a7ded6a206e
Назар Остафійчук <ostafii4uk@gmail.com>
2024-04-03 12:50:25 +0300

    Fix authentication status

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,28 +1,41 @@
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const { data, status } = useSession()
+
+  const logoutHandler = () => {
+    signOut()
+  }
+
   return (
     <header className={classes.header}>
-      <Link href="/">
+      <Link href='/'>
         <div className={classes.logo}>Next Auth</div>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!data && status !== 'loading' && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+          {data && (
+            <li>
+              <Link href='/profile'>Profile</Link>
+            </li>
+          )}
+          {data && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
-  );
+  )
 }
 
 export default MainNavigation;
